cad0ce3e3b40f0656bd7f40f255267e2ec4094f9
aglayrton <aglayrtonjuliao@gmail.com>
2023-02-23 19:31:04 -0300

    método na classe DadosCadastroMedicoxque retorna um objeto simplificado

--- src/main/java/med/growdev/api/dto/medico/DadosCadastroMedicox.java
+++ src/main/java/med/growdev/api/dto/medico/DadosCadastroMedicox.java
@@ -9,31 +9,45 @@ import med.growdev.api.dto.endereco.DadosEndereco;
 import med.growdev.api.entities.Medico;
 
 public record DadosCadastroMedicox(
-    @NotBlank
-    String nome,
-    @NotBlank
-    @Email
-    String email,
-    @NotBlank
-    @Pattern(regexp = "\\d{4,6}") //quatro a seis digitos
-    String crm,
-    @NotNull //O SPRING JA TOMA CONTA DE ENUM, E É NOTNULL PORQUE NAO É UMA STRING
-    Especialidade especialidade,
+  @NotBlank
+  String nome,
+  @NotBlank
+  @Email
+  String email,
+  @NotBlank
+  @Pattern(regexp = "\\d{4,6}") //quatro a seis digitos
+  String crm,
+  @NotNull //O SPRING JA TOMA CONTA DE ENUM, E É NOTNULL PORQUE NAO É UMA STRING
+  Especialidade especialidade,
 
-    @NotBlank
-    String telefone,
+  @NotBlank
+  String telefone,
 
-    @NotNull
-    @Valid //diz para validar esse objeto
-    DadosEndereco endereco) {
+  @NotNull
+  @Valid //diz para validar esse objeto
+  DadosEndereco endereco) {
 
   public DadosCadastroMedicox(Medico medico) {
-     this( medico.getNome(),
-       medico.getEmail(),
-       medico.getCrm(),
-       medico.getEspecialidade(),
-       medico.getTelefone(),
-       new DadosEndereco(medico.getEndereco()));
+    this(medico.getNome(),
+      medico.getEmail(),
+      medico.getCrm(),
+      medico.getEspecialidade(),
+      medico.getTelefone(),
+      new DadosEndereco(medico.getEndereco()));
+  }
+
+  // Você pode criar um novo método na classe DadosCadastroMedicoxque retorna um objeto simplificado, contendo somente os campos que você deseja exibir.
+  public SimplifiedMedico toSimplifiedMedico() {
+    return new SimplifiedMedico(nome, email, crm, especialidade);
+  }
+
+  //  Essa classe foi criada como um registro interno, que é uma classe simples que contém somente campos e nenhum método adicional.
+  public record SimplifiedMedico(
+    String nome,
+    String email,
+    String crm,
+    Especialidade especialidade
+  ) {
   }
 
 }
