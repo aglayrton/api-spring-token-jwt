4247c1af543590ad4a075f6854d79d1f0c4f4662
aglayrton <aglayrtonjuliao@gmail.com>
2023-02-22 21:56:14 -0300

    atualizações

--- src/main/java/med/growdev/api/medico/DadosCadastroMedicox.java
+++
@@ -1,6 +0,0 @@
-package med.growdev.api.medico;
-
-import med.growdev.api.endereco.DadosEndereco;
-
-public record DadosCadastroMedicox(String nome, String email, String crm, Especialidade especialidade, DadosEndereco endereco) {
-}
