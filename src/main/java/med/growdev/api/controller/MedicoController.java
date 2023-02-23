dcf6c3ac3255876252ed8e7d6a013c2f586eecc3
aglayrton <aglayrtonjuliao@gmail.com>
2023-02-23 19:31:26 -0300

    Rota Get criada

--- src/main/java/med/growdev/api/controller/MedicoController.java
+++ src/main/java/med/growdev/api/controller/MedicoController.java
@@ -6,10 +6,9 @@ import med.growdev.api.services.MedicoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -22,6 +21,12 @@ public class MedicoController {
   public ResponseEntity<DadosCadastroMedicox> cadastrar(@Valid @RequestBody DadosCadastroMedicox dados){
     DadosCadastroMedicox md = medicoService.save(dados);
     return ResponseEntity.status(HttpStatus.CREATED).body(md);
+  }
+
+  @GetMapping
+  public ResponseEntity<List<DadosCadastroMedicox.SimplifiedMedico>> listar(){
+    List<DadosCadastroMedicox.SimplifiedMedico> mdList = medicoService.getAllSimplified();
+    return ResponseEntity.ok().body(mdList);
   }
 
 }
