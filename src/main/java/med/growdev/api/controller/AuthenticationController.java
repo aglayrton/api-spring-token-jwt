e0e6e20d5205df5f21de5500ea60726a4ae51de2
aglayrton <aglayrtonjuliao@gmail.com>
2023-03-19 12:34:15 -0300

    Organização do token com DTO.

--- src/main/java/med/growdev/api/controller/AuthenticationController.java
+++ src/main/java/med/growdev/api/controller/AuthenticationController.java
@@ -3,6 +3,7 @@ package med.growdev.api.controller;
 import jakarta.validation.Valid;
 import med.growdev.api.domain.User;
 import med.growdev.api.dto.user.DadosAutenticacao;
+import med.growdev.api.infra.secutiry.DadosTokenJWT;
 import med.growdev.api.infra.secutiry.TokenService;
 import org.aspectj.weaver.patterns.IToken;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,9 +30,10 @@ public class AuthenticationController {
     @PostMapping
     public ResponseEntity login(@RequestBody @Valid DadosAutenticacao dados){
         //criando token
-        var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
-        var authentication = authenticationManager.authenticate(token);
-        return ResponseEntity.ok(tokenService.gerarToken((User) authentication.getPrincipal()));
+        var autheticationToken = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
+        var authentication = authenticationManager.authenticate(autheticationToken);
+        var tokenJWT = tokenService.gerarToken((User) authentication.getPrincipal());
+        return ResponseEntity.ok(new DadosTokenJWT(tokenJWT));
     }
 
 }
