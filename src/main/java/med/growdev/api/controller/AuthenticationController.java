c63330301890e142944de0fef7f905314a4e6ecb
aglayrton <aglayrtonjuliao@gmail.com>
2023-03-19 12:10:21 -0300

    Token sendo retornado.

--- src/main/java/med/growdev/api/controller/AuthenticationController.java
+++ src/main/java/med/growdev/api/controller/AuthenticationController.java
@@ -1,12 +1,15 @@
 package med.growdev.api.controller;
 
 import jakarta.validation.Valid;
+import med.growdev.api.domain.User;
 import med.growdev.api.dto.user.DadosAutenticacao;
+import med.growdev.api.infra.secutiry.TokenService;
 import org.aspectj.weaver.patterns.IToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,12 +23,15 @@ public class AuthenticationController {
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping
     public ResponseEntity login(@RequestBody @Valid DadosAutenticacao dados){
         //criando token
         var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
         var authentication = authenticationManager.authenticate(token);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(tokenService.gerarToken((User) authentication.getPrincipal()));
     }
 
 }
