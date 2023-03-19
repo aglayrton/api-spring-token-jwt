c94da899bcb9882deae6b38f2ba5a8d43297d472
aglayrton <aglayrtonjuliao@gmail.com>
2023-03-19 10:50:18 -0300

    Repository criado

---
+++ src/main/java/med/growdev/api/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package med.growdev.api.repositories;
+
+import med.growdev.api.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    UserDetails findByLogin(String username);
+}
