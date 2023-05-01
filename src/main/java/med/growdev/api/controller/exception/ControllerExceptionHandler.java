bd5aac283df13ef6c4a950574b49712f8fa49fc1
aglayrtondev <aglayrtonjuliao@hotmail.com.br>
2023-05-01 15:30:52 -0300

    Captura os erros e retorna um corpo para o front.

--- src/main/java/med/growdev/api/controller/exception/ControllerExceptionHandler.java
+++ src/main/java/med/growdev/api/controller/exception/ControllerExceptionHandler.java
@@ -12,19 +12,19 @@ public class ControllerExceptionHandler {
 
     /*Erro 500 é quando não é tratado o erro*/
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity notFoundExcpetion(){
+    public ResponseEntity tratarErro404(){
         return ResponseEntity.notFound().build();
     }
 
     /*Erro 400 normalmente é o de validação*/
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity badRequest(MethodArgumentNotValidException e){
+    public ResponseEntity tratarErro400(MethodArgumentNotValidException e){
         var erros = e.getFieldErrors();
         return ResponseEntity.badRequest()
-                .body(erros.stream().map(DadosErroValidação::new).toList());
+                .body(erros.stream().map(DadosErroValidacao::new).toList());
     }
-    public record DadosErroValidação(String field, String message){
-        public DadosErroValidação(FieldError error){
+    public record DadosErroValidacao(String field, String message){
+        public DadosErroValidacao(FieldError error){
             this(error.getField(), error.getDefaultMessage());
         }
     }
