856219814052b8f7accd5b6f444b836382652959
aglayrtondev <aglayrtonjuliao@hotmail.com.br>
2023-05-01 14:26:16 -0300

    Função criada para retorna somente quem estiver com true no banco de dados (tipo binario 0 ou 1).

--- src/main/java/med/growdev/api/repositories/MedicoRepository.java
+++ src/main/java/med/growdev/api/repositories/MedicoRepository.java
@@ -1,7 +1,14 @@
 package med.growdev.api.repositories;
 
 import med.growdev.api.domain.Medico;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MedicoRepository extends JpaRepository<Medico, Long> {
+	//List<Medico> findAllByAtivoTrue();
+
+	Page<Medico> findAllByAtivoTrue(Pageable page);
 }
